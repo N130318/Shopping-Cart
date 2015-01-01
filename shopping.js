f2327f0731b74d8417ef1d4af9422292dfee5e18
Namita <namita1990@gmail.com>
2015-01-01 17:38:13 +0530

    Adding quantity to items added to cart

--- shopping.js
+++ shopping.js
@@ -1,11 +1,11 @@
 /**
  * Created by namita on 1/1/15.
  */
-var shampoo = {name: 'Sunsilk', id: '1', price:599 };
-var soap = {name: 'Lux', id: '2',price:55 };
-var coffee = {name: 'Nestle', id: '3',price:100};
-var chips = {name: 'Uncle Chips', id: '4',price:60};
-var chocolate = {name: 'Dairy Milk', id: '5',price:85};
+var shampoo = {name: 'Sunsilk', id: '1', price: 599 };
+var soap = {name: 'Lux', id: '2', price: 55 };
+var coffee = {name: 'Nestle', id: '3', price: 100};
+var chips = {name: 'Uncle Chips', id: '4', price: 60};
+var chocolate = {name: 'Dairy Milk', id: '5', price: 85};
 var products = [shampoo, soap, coffee, chips, chocolate];
 var shoppingModule = angular.module('shopping', []);
 
@@ -15,7 +15,10 @@ shoppingModule.controller('ShoppingController', ["$scope", function ($scope) {
     $scope.addToCart = function (id) {
         for (var index = 0; index < $scope.products.length; index++) {
             if (products[index].id == id) {
-                $scope.cartItems.push(products[index]);
+                var productCopy = angular.copy(products[index]);
+                productCopy.quantity = 1;
+                $scope.cartItems.push(productCopy);
+
             }
         }
     }
