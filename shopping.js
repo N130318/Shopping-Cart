b5757d4aca8493d72f80ae11f2ad4ebef48f23c1
Namita <namita1990@gmail.com>
2015-01-01 19:08:46 +0530

    Adding space

--- shopping.js
+++ shopping.js
@@ -7,6 +7,7 @@ var coffee = {name: 'Nestle', id: '3', price: 100};
 var chips = {name: 'Uncle Chips', id: '4', price: 60};
 var chocolate = {name: 'Dairy Milk', id: '5', price: 85};
 var products = [shampoo, soap, coffee, chips, chocolate];
+
 var shoppingModule = angular.module('shopping', []);
 
 shoppingModule.controller('ShoppingController', ["$scope", function ($scope) {
